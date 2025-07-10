a6c8a1bb1d88d8730df917b416e80a3129054485
Igor Brizack <igor_brizack@hotmail.com>
2025-07-09 21:27:06 -0300

    feat: create payment command

---
+++ internal/usecases/payment/create_payment.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/IgorBrizack/backend-rinha-3/internal/domain/payment/dto"
+	"github.com/IgorBrizack/backend-rinha-3/internal/services"
+	"github.com/redis/go-redis/v9"
+)
+
+type CreatePaymentCommand struct {
+	cacheClient    *redis.Client
+	paymentService *services.PaymentService
+}
+
+func NewCreatePaymentCommand(cacheClient *redis.Client, paymentService *services.PaymentService) *CreatePaymentCommand {
+	return &CreatePaymentCommand{
+		cacheClient:    cacheClient,
+		paymentService: paymentService,
+	}
+}
+
+func (c *CreatePaymentCommand) Execute(payment dto.PaymentRequest) error {
+	payload, err := json.Marshal(payment)
+	if err != nil {
+		return fmt.Errorf("erro ao serializar pagamento: %w", err)
+	}
+
+	queueName := "default_queue"
+
+	return c.cacheClient.RPush(context.Background(), queueName, payload).Err()
+}
